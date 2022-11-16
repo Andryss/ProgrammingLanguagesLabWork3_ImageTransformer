65d36879a2db162fc141279a089d21cbb79cb848
Andryss <andr.krivosheev@gmail.com>
2022-11-16 12:32:08 +0300

    Invoke main function in main.c

--- solution/src/main.c
+++ solution/src/main.c
@@ -1,7 +1,37 @@
-#include <stdio.h>
+#include "utils/util.h"
+#include "image/main_executor.h"
+#include "image/formats/bmp/bmp.h"
+#include "image/transforms/rotate/rotate.h"
 
-int main( int argc, char** argv ) {
-    (void) argc; (void) argv; // supress 'unused parameters' warning
+char* execute_messages[] = {
+        [EXECUTE_OK] = "Image transformer successfully finished\n",
+        [EXECUTE_ERR] = "Some error occurred during transformer working\n"
+};
+
+int main(int argc, char** argv) {
+
+    // Arg checking
+    if (argc != 3) {
+        error("Incorrect usage. Try this: image-transformer <source image> <destination image>\n");
+        return 0;
+    }
+
+    // Invoke main function
+    enum executor_status image_execute_status = file_execute
+            (
+            argv[1],
+            argv[2],
+            bmp_read_image,
+            rotate_transform,
+            bmp_write_image
+            );
+
+    // Print computation result
+    if (image_execute_status != EXECUTE_OK) {
+        error(execute_messages[image_execute_status]);
+    } else {
+        info(execute_messages[EXECUTE_OK]);
+    }
 
     return 0;
 }
